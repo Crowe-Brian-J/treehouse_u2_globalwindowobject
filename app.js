0a2d6b731974ff6bb326647b609431ccaa04afd0
Brian Crowe <brian.j.crowe@protonmail.com>
2025-07-03 13:15:49 -0400

    show add style

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const body = document.body
+/*const body = document.body;
 
 body.addEventListener('click', () => {
-  body.innerHTML = '<h1>Hello, World!</h1>'
-})
+  body.innerHTML = '<h1>Hello, World!</h1>';
+});*/
+
+const headline = document.getElementById('headline')
+
+headline.style.border = 'solid 2px red'
