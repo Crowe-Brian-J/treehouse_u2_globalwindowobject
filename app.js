7b501fc8e55f8f861fff5909ea6db79ce8f2df4c
Brian Crowe <brian.j.crowe@protonmail.com>
2025-07-07 16:14:28 -0400

    update index.html to show text input field, update app.js to handle text input field, comment out code that showed how to handle other clicks

--- app.js
+++ app.js
@@ -4,31 +4,37 @@ body.addEventListener('click', () => {
   body.innerHTML = '<h1>Hello, World!</h1>';
 });*/
 
-//find the headline element
-const headline = document.getElementById('headline')
 //call specific button on click
 const updateButton = document.querySelector('.btn-main')
-//get li elements to hide
+/*//get li elements to hide
 const liHide = document.querySelectorAll('li')
 //call hide list btn-toggle
 const toggleButton = document.getElementsByClassName('btn-toggle')[0]
 //get highlights from li items
-const highlights = document.querySelectorAll('.highlights')
+const highlights = document.querySelectorAll('.highlights')*/
 
-for (const highlight of highlights) {
-  highlight.style.backgroundColor = 'cornsilk'
-}
+/*for (const highlight of highlights) {
+  highlight.style.backgroundColor = "cornsilk";
+}*/
 
 //handle update heading button
 const handleClickHeadingBtn = () => {
-  headline.style.border = 'solid 2px red'
+  //find the headline element
+  const headline = document.getElementById('headline')
+  const input = document.querySelector('.input-main')
+  /*headline.style.border = 'solid 2px red';
+  headline.style.fontSize = '60px';*/
+  //set headline's content to input value
+  headline.textContent = input.value
+  //clear input value for next change
+  input.value = ''
 }
 //handle hide list button
-const handleHideListBtn = () => {
+/*const handleHideListBtn = () => {
   for (let i = 0; i < liHide.length; i++) {
     liHide[i].style.color = 'orchid'
   }
-}
+}*/
 
 updateButton.addEventListener('click', handleClickHeadingBtn)
-toggleButton.addEventListener('click', handleHideListBtn)
+//toggleButton.addEventListener('click', handleHideListBtn)
